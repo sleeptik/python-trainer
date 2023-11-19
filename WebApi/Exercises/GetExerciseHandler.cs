8beb286b622b813d7074e64d24aa4233c396c5f6
ArterialOrc <stud0000247851@study.utmn.ru>
2023-11-19 18:31:56 +0500

    get Exercise by subject and difficulty

--- WebApi/Exercises/GetExerciseHandler.cs
+++ WebApi/Exercises/GetExerciseHandler.cs
@@ -1,13 +1,17 @@
 ﻿using Domain.Trainer;
 using Infrastructure;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApi.Exercises;
 
-public class GetExerciseHandler(ApplicationDbContext context) : IRequestHandler<GetExerciseRequest,IList<Exercise>>
+public class GetExerciseHandler(ApplicationDbContext context) : IRequestHandler<GetExerciseRequest, IList<Exercise>>
 {
-    public Task<IList<Exercise>> Handle(GetExerciseRequest request, CancellationToken cancellationToken)
+    public async Task<IList<Exercise>> Handle(GetExerciseRequest request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return await context.Exercises
+            .Where(exercise => exercise.DifficultyId == request.DifficultyId)
+            .Where(exercise => exercise.Subjects.Any(subject => subject.Id == request.SubjectId))
+            .ToListAsync(cancellationToken: cancellationToken);
     }
 }
