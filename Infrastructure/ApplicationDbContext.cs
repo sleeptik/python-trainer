f22870a7e08acff26b756f99868d6b0f04da1daa
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-11-19 16:40:47 +0500

    supress null in ApplicationDbContext

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -5,13 +5,12 @@ namespace Infrastructure;
 
 public sealed class ApplicationDbContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<Subject> Subjects { get; private set; } = null!; ;
+    public DbSet<Exercise> Exercises { get; private set; } = null!;
+    public DbSet<Difficulty> Difficulties { get; private set; } = null!;
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
-    
-    public DbSet<Exercise> Exercises { get; private set; }
-    public DbSet<Difficulty> Difficulties { get; private set; }
-    public DbSet<Subject> Subjects { get; private set; }
-
 }
