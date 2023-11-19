5d9d3351b432202538e7e64f1b06153453212eb2
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-11-19 15:25:54 +0500

    add npgsql, add DI extensions for Infrastructure

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Default");
+        services.AddDbContext<ApplicationDbContext>(builder => builder.UseNpgsql(connectionString));
+    }
+}
