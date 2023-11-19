bcc8359cd4afa7dd63cd3faaaedc1376bccaaff9
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-11-19 16:45:41 +0500

    add private setter to exercise id, add difficulty entity

--- Domain/Trainer/Exercise.cs
+++ Domain/Trainer/Exercise.cs
@@ -2,7 +2,8 @@
 
 public sealed class Exercise
 {
-    public int Id { get; }
+    public int Id { get; private set; }
     public string Contents { get; private set; } = null!;
-    public Difficulty Difficulty { get; private set; }
+
+    public Difficulty Difficulty { get; private set; } = null!;
 }
