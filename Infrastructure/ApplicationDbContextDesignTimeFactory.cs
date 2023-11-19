5718c3de97756ca2a34e77d6ce15efbc6a95c6e5
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-11-19 16:48:54 +0500

    add design time db factory

---
+++ Infrastructure/ApplicationDbContextDesignTimeFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure;
+
+// ReSharper disable once UnusedType.Global
+public sealed class ApplicationDbContextDesignTimeFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseNpgsql("Server=localhost;Port=5432;Database=trainer;UserId=postgres;Password=postgres;")
+            .Options;
+
+        return new ApplicationDbContext(options);
+    }
+}
