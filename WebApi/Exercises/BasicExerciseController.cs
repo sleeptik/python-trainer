83524ff2625760eed1bea765b40891d2647f5e8d
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-11-20 08:17:57 +0500

    reformat file

--- WebApi/Exercises/BasicExerciseController.cs
+++ WebApi/Exercises/BasicExerciseController.cs
@@ -3,14 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using WebApi.Common;
 
 namespace WebApi.Exercises;
+
 [Route("education")]
 public sealed class BasicExerciseController(ISender sender) : ApiController
 {
     [HttpGet("new")]
-    public async Task<IActionResult> GetExerciseBySubjectAndDifficulty(int subjectId,int difficultyId)
+    public async Task<IActionResult> GetExerciseBySubjectAndDifficulty(int subjectId, int difficultyId)
     {
-        var exercises = await sender.Send(new GetExerciseRequest(subjectId,difficultyId));
+        var exercises = await sender.Send(new GetExerciseRequest(subjectId, difficultyId));
         return Ok(exercises);
     }
-
 }
