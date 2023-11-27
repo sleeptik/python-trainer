c017f3dc6d90a6a2d97628285547992edc136b1f
ArterialOrc <stud0000247851@study.utmn.ru>
2023-11-27 21:17:07 +0500

    Add UserRankService

---
+++ WebApi/Education/UserRankService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Users;
+using Infrastructure;
+
+namespace WebApi.Education;
+
+public class UserRankService(ApplicationDbContext context)
+{
+    public UserRank GetUserRank(int userId)
+    {
+        return context.UserRanks
+            .First(rank => rank.UserId==userId);
+    }
+}
