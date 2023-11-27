473d443594c13283559a4184b1132c17d95af96c
ArterialOrc <stud0000247851@study.utmn.ru>
2023-11-27 21:11:42 +0500

    Add HistoryService for retrieving userHistory

---
+++ WebApi/Education/HistoryService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Trainer;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Education;
+
+public class HistoryService(ApplicationDbContext context)
+{
+    public IList<History> GetUserHistory(int userId)
+    {
+        return context.Histories.AsNoTracking()
+            .Include(history => history.Exercise)
+            .Where(history => history.UserId == userId)
+            .OrderBy(history => history.Finished)
+            .ToList();
+    }
+}
