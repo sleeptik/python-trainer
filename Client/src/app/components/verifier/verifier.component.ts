1177b2c3d63421b22c238545d9ee5e9ea903e4a8
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-11-28 21:43:11 +0500

    update verifier component

--- Client/src/app/components/verifier/verifier.component.ts
+++ Client/src/app/components/verifier/verifier.component.ts
@@ -1,9 +1,13 @@
 import {Component} from '@angular/core';
+import {EducationAdminService} from "../../services/education-admin.service";
 
 @Component({
   selector: 'app-verifier',
   templateUrl: './verifier.component.html'
 })
 export class VerifierComponent {
+  history: any | undefined;
 
+  constructor(private readonly educationAdminService: EducationAdminService) {
+  }
 }
