ccb3c418ec72ffe1221ec57db34fac679403ef6f
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-12-02 16:08:12 +0500

    update history model
    remove passed status from constructor
    add SetCompletion function
    remove redundant null assignments

--- Domain/Trainer/History.cs
+++ Domain/Trainer/History.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain.Trainer;
 
-public sealed class History(int userId, int exerciseId, bool isPassed)
+public sealed class History(int userId, int exerciseId)
 {
     public int StudentId { get; private set; } = userId;
     public Student Student { get; private set; } = null!;
@@ -8,8 +8,19 @@ public sealed class History(int userId, int exerciseId, bool isPassed)
     public int ExerciseId { get; private set; } = exerciseId;
     public Exercise Exercise { get; private set; } = null!;
 
-    public bool? IsPassed { get; private set; } = isPassed;
+    public bool? IsPassed { get; private set; }
 
     public DateTime AssignedAt { get; private set; } = DateTime.UtcNow;
-    public DateTime? FinishedAt { get; private set; } = null;
+    public DateTime? FinishedAt { get; private set; }
+
+    public void SetCompletion(bool isPassed)
+    {
+        IsPassed = IsPassed is null
+            ? isPassed
+            : throw new InvalidOperationException("Updating completion status is forbidden");
+
+        FinishedAt = FinishedAt is null
+            ? DateTime.UtcNow
+            : throw new InvalidOperationException("Updating finish date is forbidden");
+    }
 }
