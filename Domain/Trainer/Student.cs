7cdcdc4e08f1a7ac37295bf18f9c46c3dcb3b1cc
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-12-02 15:51:27 +0500

    add student model

---
+++ Domain/Trainer/Student.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Users;
+
+namespace Domain.Trainer;
+
+public class Student(int userId)
+{
+    public int UserId { get; private set; } = userId;
+    public User User { get; private set; } = null!;
+
+    public int CurrentDifficultyId { get; private set; } = default;
+    public Difficulty CurrentDifficulty { get; private set; } = null!;
+
+    public int Score { get; private set; } = default;
+}
