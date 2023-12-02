3e09cd2bd14e2938a7c79df9ac5d20862cb2fed3
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-12-02 15:52:53 +0500

    add student configuration

---
+++ Infrastructure/Configurations/StudentConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Trainer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class StudentConfiguration : IEntityTypeConfiguration<Student>
+{
+    public void Configure(EntityTypeBuilder<Student> builder)
+    {
+        builder.HasKey(student => student.UserId);
+    }
+}
