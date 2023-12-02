3055f2c84d3827a9093dd6b3644d9e9680e57637
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-12-02 16:57:03 +0500

    update SetHistoryStatusHandler to SetAssignmentResultHandler

---
+++ WebApi/Features/EducationAdmin/Handlers/SetAssignmentResultHandler.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure;
+using MediatR;
+using WebApi.Features.EducationAdmin.Requests;
+
+namespace WebApi.Features.EducationAdmin.Handlers;
+
+public class SetAssignmentResultHandler(ApplicationDbContext context) : IRequestHandler<SetHistoryStatusCommand>
+{
+    public async Task Handle(SetHistoryStatusCommand request, CancellationToken cancellationToken)
+    {
+        var assignment = (await context.Assignments.FindAsync(request.UserId, request.ExerciseId))!;
+
+        assignment.SetResult(request.Status);
+
+        context.Assignments.Update(assignment);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
