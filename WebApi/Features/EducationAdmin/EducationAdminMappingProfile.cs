3e4b2173143ec4b86bbb78f505a60a0cdce04b59
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-12-02 17:36:07 +0500

    add mapping profile

---
+++ WebApi/Features/EducationAdmin/EducationAdminMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain.Trainer;
+using WebApi.Features.EducationAdmin.Response;
+
+namespace WebApi.Features.EducationAdmin;
+
+public class EducationAdminMappingProfile : Profile
+{
+    public EducationAdminMappingProfile()
+    {
+        CreateMap<Assignment, GetUnverifiedAssignmentResponse>();
+    }
+}
