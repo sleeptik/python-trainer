a14ff336166bb01627776ee5bbdec18fd6eaaacb
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-12-02 17:15:28 +0500

    update notification and use it

--- WebApi/Features/EducationAdmin/EducationAdminController.cs
+++ WebApi/Features/EducationAdmin/EducationAdminController.cs
@@ -1,24 +1,29 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Common;
+using WebApi.Features.EducationAdmin.Notifications;
 using WebApi.Features.EducationAdmin.Requests;
 
 namespace WebApi.Features.EducationAdmin;
 
 [Route("api/admin/education")]
-public class EducationAdminController(ISender sender) : ApiController
+public class EducationAdminController(IMediator mediator) : ApiController
 {
     [HttpGet("status")]
     public async Task<IActionResult> GetUnverifiedExercises([FromQuery] GetAssignmentStatusRequest request)
     {
-        var exercises = await sender.Send(request);
+        var exercises = await mediator.Send(request);
         return Ok(exercises);
     }
 
     [HttpPut("status")]
-    public async Task<IActionResult> SetStatus(SetHistoryStatusCommand statusCommand)
+    public async Task<IActionResult> SetStatus(SetHistoryStatusCommand command)
     {
-        await sender.Send(statusCommand);
+        await mediator.Send(command);
+
+        var notification = new AssignmentVerifiedNotification(command.UserId, command.ExerciseId, command.Status);
+        await mediator.Publish(notification);
+
         return NoContent();
     }
 }
