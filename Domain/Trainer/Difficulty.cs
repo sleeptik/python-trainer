afb9155b5b723fa3a462cf0ded9aae9e4ba0bc2d
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-12-02 15:51:09 +0500

    update difficulty model
    add limiting bounds
    add lower bound threshold

--- Domain/Trainer/Difficulty.cs
+++ Domain/Trainer/Difficulty.cs
@@ -2,6 +2,11 @@
 
 public sealed class Difficulty
 {
-    public int Id { get; private set; }
+    public int Id { get; private set; } = default;
     public string Name { get; private set; } = null!;
+
+    public float LowerBound { get; private set; } = default;
+    public float UpperBound { get; private set; } = default;
+
+    public float FallThreshold { get; private set; } = default;
 }
