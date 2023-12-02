a1ce35c5c64101a006d98e6347206eb392bd537b
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2023-12-02 21:52:05 +0500

    add logic to change score, add unit tests

---
+++ Domain.Unit/Trainer/StudentTests.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Trainer;
+using FluentAssertions;
+
+namespace Domain.Unit.Trainer;
+
+public class StudentTests
+{
+    private readonly Student _student = new(default);
+
+    [Theory]
+    [InlineData(25)]
+    [InlineData(50)]
+    [InlineData(75)]
+    [InlineData(100)]
+    public void AddsToScore(float change)
+    {
+        _student.Score += change;
+        _student.Score.Should().Be(change);
+    }
+
+    [Theory]
+    [InlineData(-25)]
+    [InlineData(-50)]
+    [InlineData(-75)]
+    [InlineData(-100)]
+    public void ClampsScoreToZero(float change)
+    {
+        _student.Score += change;
+        _student.Score.Should().Be(0f);
+    }
+}
--- Domain/Trainer/Student.cs
+++ Domain/Trainer/Student.cs
@@ -5,6 +5,7 @@ namespace Domain.Trainer;
 public sealed class Student(int userId)
 {
     private readonly IList<Assignment> _assignments = new List<Assignment>();
+    private float _score;
 
     public int UserId { get; private set; } = userId;
     public User User { get; private set; } = null!;
@@ -12,7 +13,11 @@ public sealed class Student(int userId)
     public int CurrentRankId { get; private set; } = default;
     public Rank CurrentRank { get; private set; } = null!;
 
-    public int Score { get; private set; } = default;
+    public float Score
+    {
+        get => _score;
+        set => _score = Math.Max(0, value);
+    }
 
     public IList<Assignment> Assignments => _assignments.AsReadOnly();
 }
