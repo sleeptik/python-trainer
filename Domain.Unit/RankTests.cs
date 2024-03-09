fd88c9da5170521278914f6100a3a5fd6157f916
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-03-09 16:00:58 +0500

    add factory method and tests to rank entity

---
+++ Domain.Unit/RankTests.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Trainer;
+
+namespace Domain.Unit;
+
+public class RankTests
+{
+    [Fact]
+    public void RankCreate_ValidData_ReturnsRank()
+    {
+        var rank = Rank.Create("name", 0, 1, 1);
+        rank.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void RankCreate_InvalidBounds_ThrowsException()
+    {
+        Action create = () => Rank.Create("name", 2, 1, 1);
+        create.Invoking(action => action.Invoke()).Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void RankCreate_EmptyName_ThrowsException()
+    {
+        Action create = () => Rank.Create("", 0, 1, 1);
+        create.Invoking(action => action.Invoke()).Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void RankCreate_NullName_ThrowsException()
+    {
+        Action create = () => Rank.Create(null, 0, 1, 1);
+        create.Invoking(action => action.Invoke()).Should().Throw<ArgumentException>();
+    }
+}
--- Domain/Trainer/Rank.cs
+++ Domain/Trainer/Rank.cs
@@ -1,12 +1,35 @@
-﻿namespace Domain.Trainer;
+﻿// ReSharper disable UnusedAutoPropertyAccessor.Local
+
+namespace Domain.Trainer;
 
 public sealed class Rank
 {
-    public int Id { get; private set; } = default;
+    private Rank()
+    {
+    }
+
+    public int Id { get; private set; }
     public string Name { get; private set; } = null!;
 
-    public float LowerBound { get; private set; } = default;
-    public float UpperBound { get; private set; } = default;
+    public float LowerBound { get; private set; }
+    public float UpperBound { get; private set; }
 
-    public float FallThreshold { get; private set; } = default;
+    public float FallThreshold { get; private set; }
+
+    public static Rank Create(string name, float lowerBound, float upperBound, float fallThreshold)
+    {
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentException("Name is null or empty");
+
+        if (lowerBound > upperBound)
+            throw new ArgumentException("Lower bound greater than upper bound");
+
+        return new Rank
+        {
+            Name = name,
+            LowerBound = lowerBound,
+            UpperBound = upperBound,
+            FallThreshold = fallThreshold
+        };
+    }
 }
