27a3896999ca227cc9a3943a72dbe5b85cc0cbe4
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-04-08 00:50:52 +0500

    update result component

--- Client/src/app/components/trainer-result/trainer-result.component.ts
+++ Client/src/app/components/trainer-result/trainer-result.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-trainer-result',
   templateUrl: './trainer-result.component.html'
 })
 export class TrainerResultComponent {
-
+  @Input() result!: string | null;
 }
