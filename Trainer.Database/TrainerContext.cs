e83dd2134687a4b0ba6a771268fb5b45c31377f9
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-05-16 17:54:33 +0500

    update context to identity db context

--- Trainer.Database/TrainerContext.cs
+++ Trainer.Database/TrainerContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Trainer.Database.Entities.Assignments;
 using Trainer.Database.Entities.Auth;
 using Trainer.Database.Entities.Exercises;
@@ -6,15 +7,18 @@ using Trainer.Database.Entities.Students;
 
 namespace Trainer.Database;
 
-public sealed class TrainerContext(DbContextOptions options) : DbContext(options)
+public sealed class TrainerContext(DbContextOptions options) : IdentityDbContext<User, Role, int>(options)
 {
     public DbSet<Assignment> Assignments => Set<Assignment>();
-    public DbSet<Student> Students => Set<Student>();
+
     public DbSet<Exercise> Exercises => Set<Exercise>();
     public DbSet<Subject> Subjects => Set<Subject>();
+
+    public DbSet<Student> Students => Set<Student>();
     public DbSet<Rank> Ranks => Set<Rank>();
 
     public DbSet<User> Users => Set<User>();
+    public DbSet<Role> Roles => Set<Role>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
