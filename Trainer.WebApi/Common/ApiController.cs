c6566f7d8bd9298c8ce5f67a0c4a573476618d8f
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-05-16 19:07:40 +0500

    update base api controller

--- Trainer.WebApi/Common/ApiController.cs
+++ Trainer.WebApi/Common/ApiController.cs
@@ -1,6 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Trainer.Database;
 
 namespace Trainer.WebApi.Common;
 
 [ApiController]
-public class ApiController : ControllerBase;
+public class ApiController : ControllerBase
+{
+    private TrainerContext? _trainerContext;
+
+    protected TrainerContext TrainerContext
+    {
+        get
+        {
+            _trainerContext ??= HttpContext.RequestServices.GetRequiredService<TrainerContext>();
+            return _trainerContext;
+        }
+    }
+}
