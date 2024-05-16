bc2e98c32d35967f2be5e155359c64bb42396a7d
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-05-16 17:17:39 +0500

    move context to other project
    - update dependency injection extensions

---
+++ Trainer.Database/TrainerContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trainer.Database.Entities.Assignments;
+using Trainer.Database.Entities.Exercises;
+using Trainer.Database.Entities.Students;
+using Trainer.Database.Entities.Users;
+
+namespace Trainer.Database;
+
+public sealed class TrainerContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Assignment> Assignments => Set<Assignment>();
+    public DbSet<Student> Students => Set<Student>();
+    public DbSet<Exercise> Exercises => Set<Exercise>();
+    public DbSet<Subject> Subjects => Set<Subject>();
+    public DbSet<Rank> Ranks => Set<Rank>();
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TrainerContext).Assembly);
+    }
+}
