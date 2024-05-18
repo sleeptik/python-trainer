86da98387a73aa5b1b9404d1143a3d8d24401ea1
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-05-18 11:53:35 +0500

    bring back old files

---
+++ Trainer.WebApi/Features/Education/GetAssignmentDetails/GetAssignmentDetailsRequest.cs
@@ -0,0 +1,6 @@
+﻿using Domain.Trainer;
+using MediatR;
+
+namespace Trainer.WebApi.Features.Education.GetAssignmentDetails;
+
+public record GetAssignmentDetailsRequest(int StudentId, int ExerciseId) : IRequest<Assignment>;
