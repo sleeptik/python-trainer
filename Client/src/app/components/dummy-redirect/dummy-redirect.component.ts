8af9065b45f8f4f1b82ff0afa230749e4d23aa04
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-05-19 19:29:59 +0500

    add redirectable component

---
+++ Client/src/app/components/dummy-redirect/dummy-redirect.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy-redirect',
+  templateUrl: './dummy-redirect.component.html'
+})
+export class DummyRedirectComponent {
+}
---
+++ Client/src/app/guards/yandex-login-redirect.guard.ts
@@ -0,0 +1,22 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {AuthService} from "../services/auth.service";
+import {switchMap} from "rxjs";
+
+export const yandexLoginRedirectGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  const codeString = route.queryParamMap.get("code");
+
+  if (codeString === null) {
+    return router.navigateByUrl("/");
+  }
+
+  const code = parseInt(codeString!);
+
+  return authService.login(code).pipe(
+    switchMap(value => router.navigateByUrl(value ? "/trainer" : "/")
+    )
+  );
+};
