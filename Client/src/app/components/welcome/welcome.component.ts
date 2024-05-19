9394f4a0291352e2182bf146c39fa79e6a11dfd5
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-05-19 18:00:05 +0500

    add auth redirect to welcome component

--- Client/src/app/components/welcome/welcome.component.ts
+++ Client/src/app/components/welcome/welcome.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html'
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+  authLink: string | null = null;
 
+  constructor(private readonly authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authService.getYandexRedirectLink().subscribe(value => this.authLink = value);
+  }
 }
