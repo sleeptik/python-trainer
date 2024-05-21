5e8689e41f0dd8caea0f9c5b3b6a6c3372db2c41
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-05-21 11:48:27 +0500

    update welcome component

--- Client/src/app/components/welcome/welcome.component.ts
+++ Client/src/app/components/welcome/welcome.component.ts
@@ -1,17 +1,27 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
+import {BehaviorSubject} from "rxjs";
+import {SimpleUserInfo} from "../../models/simple-user-info";
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html'
 })
 export class WelcomeComponent implements OnInit {
+  isLoading = true;
+  currentUser$ = new BehaviorSubject<SimpleUserInfo | null>(null);
+
   authLink: string | null = null;
 
   constructor(private readonly authService: AuthService) {
   }
 
   ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe(value => {
+      this.currentUser$.next(value);
+      this.isLoading = false;
+    });
+
     this.authService.getYandexRedirectLink().subscribe(value => this.authLink = value);
   }
 }
