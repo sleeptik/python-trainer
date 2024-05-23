7f27757c40ce3a08822a7f8d74831a5e4b071b1d
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-05-24 00:44:04 +0500

    add button handling

--- Client/src/app/components/assignments/assignments.component.ts
+++ Client/src/app/components/assignments/assignments.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Assignment} from "../../models/assignment";
 import {ActivatedRoute} from "@angular/router";
 import {Subject} from "../../models/subject";
+import {EducationService} from "../../services/education.service";
 
 @Component({
   selector: 'app-assignments',
@@ -11,8 +12,18 @@ export class AssignmentsComponent {
   assignments: Assignment[] = [];
   subjects: Subject[];
 
-  constructor(activatedRoute: ActivatedRoute) {
+  constructor(activatedRoute: ActivatedRoute,
+              private readonly educationService: EducationService) {
     this.assignments = activatedRoute.snapshot.data["assignments"];
     this.subjects = activatedRoute.snapshot.data["subjects"];
+  }
+
+  assignRandomSubjectExercise() {
+    const subjectId = Math.random() * (this.subjects.length - 1);
+    this.educationService.selfAssignNewExercise(subjectId);
+  }
+
+  assignSelectedSubjectExercise(subjectId: number) {
+    this.educationService.selfAssignNewExercise(subjectId);
   }
 }
