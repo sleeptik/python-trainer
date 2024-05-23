667614d6f3e61839c0ed521a18732233f2e87dfb
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-05-24 00:39:16 +0500

    update button handling in assignments menu

--- Client/src/app/components/assignments-menu/assignments-menu.component.ts
+++ Client/src/app/components/assignments-menu/assignments-menu.component.ts
@@ -1,14 +1,28 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Subject} from "../../models/subject";
 import {Assignment} from "../../models/assignment";
+import {FormBuilder, FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-assignments-menu',
   templateUrl: './assignments-menu.component.html'
 })
-export class AssignmentsMenuComponent {
+export class AssignmentsMenuComponent implements OnInit {
   @Input({required: true}) subjects!: Subject[];
   @Input({required: true}) assignments!: Assignment[];
+
+  @Output() randomClicked = new EventEmitter<unknown>();
+  @Output() selectedClicked = new EventEmitter<Subject>();
+
+  selectForm: FormControl<Subject | null>;
+
+  constructor(private readonly formBuilder: FormBuilder) {
+    this.selectForm = this.formBuilder.control(null);
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => this.selectForm = this.formBuilder.control(this.subjects[0]));
+  }
 
   get total(): number {
     return this.assignments.length;
