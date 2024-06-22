8a0a1fc31d68edce28f9e7fca7d91c207f3945f7
Ilya Nazarov <66788142+sleeptik@users.noreply.github.com>
2024-06-22 16:06:10 +0500

    delete unused components

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,33 +1,31 @@
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
-import {AppComponent} from './app.component';
+import {AppComponent} from "./app.component";
 import {AppRoutingModule} from "./app-routing.module";
-import {HomeComponent} from './components/home/home.component';
-import {NavMenuComponent} from './components/nav-menu/nav-menu.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CommonModule} from "@angular/common";
-import {WelcomeComponent} from './components/welcome/welcome.component';
-import {TrainerLayoutComponent} from './components/trainer-layout/trainer-layout.component';
-import {TrainerInformationComponent} from './components/trainer-information/trainer-information.component';
-import {TrainerEditorComponent} from './components/trainer-editor/trainer-editor.component';
-import {TrainerComponent} from './components/trainer/trainer.component';
+import {WelcomeComponent} from "./components/welcome/welcome.component";
+import {TrainerLayoutComponent} from "./components/trainer-layout/trainer-layout.component";
+import {TrainerInformationComponent} from "./components/trainer-information/trainer-information.component";
+import {TrainerEditorComponent} from "./components/trainer-editor/trainer-editor.component";
+import {TrainerComponent} from "./components/trainer/trainer.component";
 import {MonacoEditorModule} from "ngx-monaco-editor-v2";
-import {TrainerMenuComponent} from './components/trainer-menu/trainer-menu.component';
+import {TrainerMenuComponent} from "./components/trainer-menu/trainer-menu.component";
 import {CdkAccordionModule} from "@angular/cdk/accordion";
-import {TrainerResultComponent} from './components/trainer-result/trainer-result.component';
-import {TrainerOutputComponent} from './components/trainer-output/trainer-output.component';
-import {AssignmentsComponent} from './components/assignments/assignments.component';
-import {AssignmentsTableComponent} from './components/assignments-table/assignments-table.component';
+import {TrainerResultComponent} from "./components/trainer-result/trainer-result.component";
+import {TrainerOutputComponent} from "./components/trainer-output/trainer-output.component";
+import {AssignmentsComponent} from "./components/assignments/assignments.component";
+import {AssignmentsTableComponent} from "./components/assignments-table/assignments-table.component";
 import {CdkTableModule} from "@angular/cdk/table";
-import {AssignmentsMenuComponent} from './components/assignments-menu/assignments-menu.component';
-import {DummyRedirectComponent} from './components/dummy-redirect/dummy-redirect.component';
-import {TrainerLoadingOverlayComponent} from './components/trainer-loading-overlay/trainer-loading-overlay.component';
+import {AssignmentsMenuComponent} from "./components/assignments-menu/assignments-menu.component";
+import {DummyRedirectComponent} from "./components/dummy-redirect/dummy-redirect.component";
+import {TrainerLoadingOverlayComponent} from "./components/trainer-loading-overlay/trainer-loading-overlay.component";
 import {MatProgressBar} from "@angular/material/progress-bar";
 import {MathjaxModule} from "mathjax-angular";
-import {TrainerTemplateLoaderComponent} from './components/trainer-template-loader/trainer-template-loader.component';
+import {TrainerTemplateLoaderComponent} from "./components/trainer-template-loader/trainer-template-loader.component";
 
 const MathJaxConfig = {
   "config": {
@@ -46,8 +44,6 @@ const MathJaxConfig = {
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NavMenuComponent,
     WelcomeComponent,
     TrainerLayoutComponent,
     TrainerInformationComponent,
